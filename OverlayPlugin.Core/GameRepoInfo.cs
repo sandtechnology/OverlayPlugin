37eea588548ee070abe27445e4157f4d917e190e
sandtechnology <a1294790523@hotmail.com>
2026-05-09 17:26:59 +0800

    cn 7.50a and bump to 26.5.9

--- OverlayPlugin.Core/GameRepoInfo.cs
+++ OverlayPlugin.Core/GameRepoInfo.cs
@@ -8,19 +8,19 @@ namespace RainbowMage.OverlayPlugin
 {
     public class GameRepoInfo
     {
-        //CN 7.45b
-        public static readonly Version version = new Version(7, 4);
-        public static readonly int ActorMoveOpcode = 0x02B0;
-        public static readonly int ActorSetPosOpcode = 0x0085;
-        public static readonly int BattleTalk2Opcode = 0x0345;
-        public static readonly int CEDirectorOpcode = 0x0224;
-        public static readonly int CountdownOpcode = 0x0206;
-        public static readonly int CountdownCancelOpcode = 0x0196;
-        public static readonly int MapEffectOpcode = 0x02EA;
-        public static readonly int NpcYellOpcode = 0x022C;
-        public static readonly int RSVDataOpcode = 0x01D1;
-        public static readonly int MapEffect4Opcode = 0x02CE;
-        public static readonly int MapEffect8Opcode = 0x018E;
-        public static readonly int MapEffect12Opcode = 0x01B6;
+        //CN 7.50a
+        public static readonly Version version = new Version(7, 5);
+        public static readonly int ActorMoveOpcode = 0x0095;
+        public static readonly int ActorSetPosOpcode = 0x01C0;
+        public static readonly int BattleTalk2Opcode = 0x028C;
+        public static readonly int CEDirectorOpcode = 0x016C;
+        public static readonly int CountdownOpcode = 0x033F;
+        public static readonly int CountdownCancelOpcode = 0x0300;
+        public static readonly int MapEffectOpcode = 0x0338;
+        public static readonly int NpcYellOpcode = 0x02A7;
+        public static readonly int RSVDataOpcode = 0x01BB;
+        public static readonly int MapEffect4Opcode = 0x03A6;
+        public static readonly int MapEffect8Opcode = 0x0360;
+        public static readonly int MapEffect12Opcode = 0x02D8;
     }
 }
